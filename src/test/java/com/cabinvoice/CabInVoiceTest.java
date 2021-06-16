fbbbe781bf81303ceeb6423fc1166784084afa17
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-16 19:35:30 +0530

    Add:adding UC1

---
+++ src/test/java/com/cabinvoice/CabInVoiceTest.java
@@ -0,0 +1,22 @@
+package com.cabinvoice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInVoiceTest {
+    InVoiceGenerator inVoiceGenerator = new InVoiceGenerator();
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = inVoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(25,fare,0.0);
+    }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare () {
+        double distance = 0.1;
+        int time = 1;
+        double fare = inVoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
+}
